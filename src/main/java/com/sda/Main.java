c97890bf07048e43187051cfb8236bfe23960f5c
PPiynio <iworkshop.pl@gmail.com>
2023-05-13 10:25:26 +0200

    first commit

---
+++ src/main/java/com/sda/Main.java
@@ -0,0 +1,10 @@
+package com.sda;
+
+public class Main {
+    public static void main(String[] args) {
+        if (true){
+            System.out.println("blabla");
+        }
+        System.out.println("test");
+    }
+}
